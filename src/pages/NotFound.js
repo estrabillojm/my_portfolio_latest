3ec7ee9f5b8ab0b65d7f53fa29910cd69f493c43
jm <estrabillo.johnmichael@gmail.com>
2022-01-04 00:12:29 +0800

    Adding 404 Page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+import "./styles/notFound.css"
+
 const NotFound = () => {
     return ( 
-        <div>
-            <h2>404 Not Found</h2>
+        <div class="container-404">
+            <h2>404</h2>
+            <h3>Request Not Found</h3>
+            <Link to="/" className="homepage-404">Back to Homepage</Link>
         </div>
      );
 }
